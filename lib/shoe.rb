c7bcd19bc74933658ddb71953f2e647634aadf75
Mark Pearyer <marcusp619@gmail.com>
2019-05-03 22:00:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,41 @@
 # Make your shoe class here!
+
+class Shoe 
+
+  def initialize(brand)  
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end 
+  
+  def color=(color)
+    @color = color
+  end
+  
+  def color 
+    @color
+  end
+  
+  def size
+    
+  end
+  
+  def material
+    
+  end
+
+  def condition
+    
+  end
+  
+  def cobble
+    puts "The shoe has been repaired"
+  end
+  
+end
+
+
+
+rspec ./spec/02_shoe_spec.rb:43 # Shoe #cobble makes the shoe's cond
