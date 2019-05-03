8b1d6c662ae486de363e3cd07fc740f3b954b32c
Mark Pearyer <marcusp619@gmail.com>
2019-05-03 21:50:32 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,14 @@
 class Book 
-  def initialize(title, author)
+  def initialize(title)
     @title = title
-    @author = author
   end
   
   def title 
     @title
+  end
+  
+  def author=(author)
+    @author = author
   end
 end
 
